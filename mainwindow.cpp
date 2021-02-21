ab110bfb2a6d6aef321d9659cd464610f46cd12e
sMeDDveD <dsoloduha@gmail.com>
2021-02-21 20:38:47 +0300

    Add realization

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,46 +1,150 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
-#include <bits/endian.h>
+#include <QColorDialog>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
 }
 
-void MainWindow::anyColorChanged(IConvertable *changer)
-{
-    rgb_color_ = changer->ToRGB();
-    cmyk_color_ = changer->ToCMYK();
-    xyz_color_ = changer->ToXYZ();
+void MainWindow::AnyColorChanged(IConvertable *changer) {
+    if (is_changing_colors_) {
+        return;
+    }
+
+    is_changing_colors_ = true;
+
+    UpdateRGB(changer);
+    UpdateCMYK(changer);
+    UpdateXYZ(changer);
 
     ui->widgetColor->setStyleSheet(QString("background-color: rgb(%1, %2, %3)")
-                                   .arg(rgb_color_.GetR())
-                                   .arg(rgb_color_.GetG())
-                                   .arg(rgb_color_.GetB()));
+                                       .arg(rgb_color_.GetR())
+                                       .arg(rgb_color_.GetG())
+                                       .arg(rgb_color_.GetB()));
+
+    is_changing_colors_ = false;
 }
 
-void MainWindow::on_sliderB_valueChanged(int value)
-{
+void MainWindow::UpdateRGB(IConvertable *from) {
+    auto updater = from->ToRGB();
+
+    ui->sliderR->setValue(updater.GetR());
+    ui->sliderG->setValue(updater.GetG());
+    ui->sliderB->setValue(updater.GetB());
+
+    rgb_color_ = updater;
+}
+
+void MainWindow::UpdateCMYK(IConvertable *from) {
+    auto updater = from->ToCMYK();
+
+    ui->sliderC->setValue(updater.GetC() * 100);
+    ui->sliderM->setValue(updater.GetM() * 100);
+    ui->sliderY->setValue(updater.GetY() * 100);
+    ui->sliderK->setValue(updater.GetK() * 100);
+
+    cmyk_color_ = updater;
+}
+
+void MainWindow::UpdateXYZ(IConvertable *from) {
+    auto updater = from->ToXYZ();
+
+    ui->sliderX->setValue(updater.GetX());
+    ui->sliderY_XYZ->setValue(updater.GetY());
+    ui->sliderZ->setValue(updater.GetZ());
+
+    xyz_color_ = updater;
+}
+
+void MainWindow::on_sliderB_valueChanged(int value) {
     rgb_color_.SetB(value);
-    anyColorChanged(&rgb_color_);
+    ui->lineEditB->setText(QString::number(value));
+
+    AnyColorChanged(&rgb_color_);
 }
 
-void MainWindow::on_sliderG_valueChanged(int value)
-{
+void MainWindow::on_sliderG_valueChanged(int value) {
     rgb_color_.SetG(value);
-    anyColorChanged(&rgb_color_);
+    ui->lineEditG->setText(QString::number(value));
+
+    AnyColorChanged(&rgb_color_);
 }
 
-void MainWindow::on_sliderR_valueChanged(int value)
-{
+void MainWindow::on_sliderR_valueChanged(int value) {
     rgb_color_.SetR(value);
-    anyColorChanged(&rgb_color_);
+    ui->lineEditR->setText(QString::number(value));
+
+    AnyColorChanged(&rgb_color_);
+}
+
+void MainWindow::on_paletteButton_clicked() {
+    auto color =
+        QColorDialog::getColor(QColor(rgb_color_.GetR(), rgb_color_.GetG(), rgb_color_.GetB()),
+                               nullptr, QString("Choose color"), QColorDialog::DontUseNativeDialog);
+
+    auto qrgb = color.rgb();
+
+    RGBColor rgb(qRed(qrgb), qGreen(qrgb), qBlue(qrgb));
+    AnyColorChanged(&rgb);
+}
+
+void MainWindow::on_sliderC_valueChanged(int value)
+{
+    cmyk_color_.SetC(value / 100.0);
+    ui->lineEditC->setText(QString::number(value));
+
+    AnyColorChanged(&cmyk_color_);
+}
+
+void MainWindow::on_sliderM_valueChanged(int value)
+{
+    cmyk_color_.SetM(value / 100.0);
+    ui->lineEditM->setText(QString::number(value));
+
+    AnyColorChanged(&cmyk_color_);
+}
+
+void MainWindow::on_sliderY_valueChanged(int value)
+{
+    cmyk_color_.SetY(value / 100.0);
+    ui->lineEditY->setText(QString::number(value));
+
+    AnyColorChanged(&cmyk_color_);
+}
+
+void MainWindow::on_sliderK_valueChanged(int value)
+{
+    cmyk_color_.SetK(value / 100.0);
+    ui->lineEditK->setText(QString::number(value));
+
+    AnyColorChanged(&cmyk_color_);
+}
+
+void MainWindow::on_sliderX_valueChanged(int value)
+{
+    xyz_color_.SetX(value);
+    ui->lineEditX->setText(QString::number(value));
+
+    AnyColorChanged(&xyz_color_);
+}
+
+void MainWindow::on_sliderY_XYZ_valueChanged(int value)
+{
+    xyz_color_.SetY(value);
+    ui->lineEditY_XYZ->setText(QString::number(value));
+
+    AnyColorChanged(&xyz_color_);
+}
+
+void MainWindow::on_sliderZ_valueChanged(int value)
+{
+    xyz_color_.SetZ(value);
+    ui->lineEditZ->setText(QString::number(value));
+
+    AnyColorChanged(&xyz_color_);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -3,13 +3,15 @@
 
 #include <QMainWindow>
 #include <colors.h>
+#include <QSlider>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui {
+class MainWindow;
+}
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
@@ -17,20 +19,40 @@ public:
     ~MainWindow();
 
 private slots:
+    void on_paletteButton_clicked();
+
     void on_sliderB_valueChanged(int value);
 
     void on_sliderG_valueChanged(int value);
 
     void on_sliderR_valueChanged(int value);
 
-private:
-    void anyColorChanged(IConvertable* changer);
+    void on_sliderC_valueChanged(int value);
 
+    void on_sliderM_valueChanged(int value);
+
+    void on_sliderY_valueChanged(int value);
+
+    void on_sliderK_valueChanged(int value);
+
+    void on_sliderX_valueChanged(int value);
+
+    void on_sliderY_XYZ_valueChanged(int value);
+
+    void on_sliderZ_valueChanged(int value);
+
+private:
+    void AnyColorChanged(IConvertable *changer);
+
+    void UpdateRGB(IConvertable *from);
+    void UpdateCMYK(IConvertable *from);
+    void UpdateXYZ(IConvertable *from);
 
     Ui::MainWindow *ui;
 
+    bool is_changing_colors_ = false;
     RGBColor rgb_color_;
     CMYKColor cmyk_color_;
     XYZColor xyz_color_;
 };
-#endif // MAINWINDOW_H
+#endif  // MAINWINDOW_H
